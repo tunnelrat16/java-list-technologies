742aa78594bde428cb48baf81f8bb362add2e68d
tunnelrat16 <tjandmatt@hotmail.com>
2022-06-01 10:11:07 -0700

    added iteration

--- MyCodingSkills.java
+++ MyCodingSkills.java
@@ -1,53 +1,18 @@
 class MyCodingSkills {
-    public static void main(String args[]) {
-        MyCodingSkills.techOne();
-        MyCodingSkills.techTwo();
-        MyCodingSkills.techThree();
-        MyCodingSkills.techFour();
-        MyCodingSkills.techFive();
-        MyCodingSkills.techSix();
-        MyCodingSkills.techSeven();
-        MyCodingSkills.techEight();
+    public static void main(String[] args) {
+        String[] skills = {
+                "During the course of the 14 weeks that we have been in the program, we started with a review of HTML5 and CSS.",
+                "Then we moved on to the CLI and Git which we used to build a website.",
+                "Next, we learned JavaScript and the proper use of the DOM.",
+                "It was during this time that we used APIs to get a better understanding of Beyonce's musical and movie library as well as a primer on Pokemon.",
+                "Our next section of training was in using TypeScript and then unit testing and analysis.",
+                "Our next few sections pivoted where we started to learn Object Oriented Programming.",
+                "The OOP led into us learning Angular and how single page applications work.",
+                "Finally over the last week, we dug in on Java, SQL and now Spring Boot as we prepare for our Capstone projects." };
+                for(int i = 0; i< skills.length; i++){
 
-    }
-
-    private static void techOne() {
-        System.out.println(
-                "During the course of the 14 weeks that we have been in the program, we started with a review of HTML5 and CSS.");
-    }
-
-    private static void techTwo() {
-        System.out.println(
-                "Then we moved on to the CLI and Git which we used to build a website.");
-    }
-
-    private static void techThree() {
-        System.out.println(
-                "Next, we learned JavaScript and the proper use of the DOM.");
-    }
-
-    private static void techFour() {
-        System.out.println(
-                "It was during this time that we used APIs to get a better understanding of Beyonce's musical and movie library as well as a primer on Pokemon.");
-    }
-
-    private static void techFive() {
-        System.out.println(
-                "Our next section of training was in using TypeScript and then unit testing and analysis.");
-    }
-
-    private static void techSix() {
-        System.out.println(
-                "Our next few sections pivoted where we started to learn Object Oriented Programming.");
-    }
-
-    private static void techSeven() {
-        System.out.println(
-                "The OOP led into us learning Angular and how single page applications work.");
-    }
-
-    private static void techEight() {
-        System.out.println(
-                "Finally over the last week, we dug in on Java, SQL and now Spring Boot as we prepare for our Capstone projects.");
+                    System.out.println(skills[i]);
+                    
+                    }
     }
 }
